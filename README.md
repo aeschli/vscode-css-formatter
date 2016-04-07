6a44733f742fa10374cc26576f47aafa0fe0d7a1
dbalage <d.balazs91@gmail.com>
2016-04-07 14:51:12 +0200

    release 0.0.1

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# VS Code CSS Formatting
+# VS Code Stylesheet Formatting
 
-This extension adds formatting to CSS.
+This extension adds formatting to CSS, SASS, LESS. 
+
+Based on [CSS Formatter](https://marketplace.visualstudio.com/items?itemName=aeschli.vscode-css-formatter) extension.
+
+Icons made by [Freepik](http://www.freepik.com) from [Flaticon](http://www.flaticon.com) is licensed by [Creative Commons BY 3.0](http://creativecommons.org/licenses/by/3.0/)
 
 ## License
 [MIT](LICENSE)
--- src/extension.ts
+++ src/extension.ts
@@ -1,55 +1,54 @@
-
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+import * as vscode from 'vscode';
 
 import jsbeautify = require('js-beautify');
 
-export function format(document: vscode.TextDocument, range: vscode.Range, options:vscode.FormattingOptions) {
-	
-	if (range === null) {
-		var start = new vscode.Position(0, 0);
-		var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-		range = new vscode.Range(start, end);
-	}
-
-	var result: vscode.TextEdit[] = [];
-	
-	var content = document.getText(range);
-	
-	if (!options) {
-		options = { insertSpaces: true, tabSize: 4 };
-	}
-	
-	var beutifyOptions : jsbeautify.IBeautifyCSSOptions = {
-		indent_char: options.insertSpaces ? ' ' : '\t',
-		indent_size: options.insertSpaces ? options.tabSize : 1,
-		selector_separator_newline: false
-	}
-	
-	var formatted = jsbeautify.css_beautify(content, beutifyOptions);
-	if (formatted) {
-		result.push(new vscode.TextEdit(range, formatted));
-	}
-	
-	return result;
-};
-
+const languageSelectors = ['css', 'sass', 'less'];
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-
-	context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider('css', {
-		provideDocumentFormattingEdits: (document, options, token) => {
-			return format(document, null, options)
-		}
-	}));
-	context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider('css', {
-		provideDocumentRangeFormattingEdits: (document, range, options, token) => {
-			var start = new vscode.Position(0, 0);
-			var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
-			return format(document, new vscode.Range(start, end), options)
-		}
-	}));	
+    context.subscriptions.push(vscode.languages.registerDocumentFormattingEditProvider(languageSelectors, {
+        provideDocumentFormattingEdits: (document, options, token) => {
+            return format(context, document, null, options)
+        }
+    }));
+    context.subscriptions.push(vscode.languages.registerDocumentRangeFormattingEditProvider(languageSelectors, {
+        provideDocumentRangeFormattingEdits: (document, range, options, token) => {
+            var start = new vscode.Position(0, 0);
+            var end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+            return format(context, document, new vscode.Range(start, end), options)
+        }
+    }));
 }
+
+export function format(context: vscode.ExtensionContext, document: vscode.TextDocument, range: vscode.Range, options: vscode.FormattingOptions) {
+
+    if (range === null) {
+        let start = new vscode.Position(0, 0);
+        let end = new vscode.Position(document.lineCount - 1, document.lineAt(document.lineCount - 1).text.length);
+        range = new vscode.Range(start, end);
+    }
+
+    let result: vscode.TextEdit[] = [];
+
+    let content = document.getText(range);
+
+    if (!options) {
+        options = { insertSpaces: true, tabSize: 4 };
+    }
+
+    let beutifyOptions: jsbeautify.IBeautifyCSSOptions = {
+        indent_char: options.insertSpaces ? ' ' : '\t',
+        indent_size: options.insertSpaces ? options.tabSize : 1,
+        selector_separator_newline: false
+    }
+
+    let formatted = jsbeautify.css_beautify(content, beutifyOptions);
+    if (formatted) {
+        result.push(new vscode.TextEdit(range, formatted));
+    }
+
+    return result;
+};
